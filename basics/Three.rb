8f8dfe602c1526be3998c2d238a5b67655839251
Oscar Daniels <oscardaniels@gmail.com>
2016-02-04 13:55:15 -0500

    completed three

---
+++ basics/Three.rb
@@ -0,0 +1,10 @@
+movies = {'star wars' => 1975, 
+          'gladiator' => 2004, 
+          'avatar' => 2013, 
+          'the last samurai' => 2001, 
+          'animal house' => 1981}
+puts movies['star wars']
+puts movies ['gladiator']
+puts movies ['avatar']
+puts movies ['the last samurai']
+puts movies ['animal house']
